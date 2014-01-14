25d20b23ccbe5117b08e026f4d13349ac86abc79
Florent Ramière <florent@ramiere.com>
2014-01-14 12:10:12 +0100

    throw exception in userservice

--- proxy/src/main/java/org/jboss/research/proxy/UserServiceImpl.java
+++ proxy/src/main/java/org/jboss/research/proxy/UserServiceImpl.java
@@ -1,15 +1,10 @@
 package org.jboss.research.proxy;
 
 public class UserServiceImpl implements UserService {
-    private static int COUNTER;
-
     @Override
     public void addUser(String userName, String userMailAddress, boolean admin) {
         // add a new user to the database
         // send a mail to invite the new user
-        COUNTER++;
-        if (COUNTER == 100) {   // nice stack trace ??
-            new Throwable().printStackTrace();
-        }
+        throw new RuntimeException("There should be a lot of noise in the stacktrace");
     }
 }
